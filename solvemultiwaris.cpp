44f49a1e4e38888c3e12cf63a111c2b4f19d0446
penyelamatbumi <inihanip7@gmail.com>
2026-05-19 20:06:28 +0700

    menambahkan class budi

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -33,3 +33,13 @@ class pelajar : public orang {
 
         }
 };
+
+class budi : public pekerja, public pelajar {
+    public : 
+        budi(int pUmur):
+            pekerja(pUmur),
+            pelajar(pUmur)
+        {
+            cout<<"budi dibuat"<< endl;
+        }
+};
