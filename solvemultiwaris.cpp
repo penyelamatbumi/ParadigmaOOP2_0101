0678de792028da939300dfba8210a6e238ee61b4
penyelamatbumi <inihanip7@gmail.com>
2026-05-19 20:04:11 +0700

    menambahkan class pekerja

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -13,3 +13,12 @@ class orang {
         }
 
 };
+
+class pekerja : public orang {
+    public:
+        pekerja(int pUmur) :
+            orang(pUmur)
+        {
+            cout << "pekerja dibuat\n"<< endl;
+        }
+};
